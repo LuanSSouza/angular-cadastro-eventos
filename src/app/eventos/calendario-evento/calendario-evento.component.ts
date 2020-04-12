48014dbfa1c0438811b99165807ac078db27c0d5
luanssouza <luan.souza27@hotmail.com>
2020-04-12 14:35:12 -0300

    atribuindo eventos a uma variável

--- src/app/eventos/calendario-evento/calendario-evento.component.ts
+++ src/app/eventos/calendario-evento/calendario-evento.component.ts
@@ -10,6 +10,9 @@ import ptbrLocale from '@fullcalendar/core/locales/pt-br';
 export class CalendarioEventoComponent implements OnInit {
   calendarPlugins = [dayGridPlugin];
   locales = [ptbrLocale];
+  eventos = [ {title: 'Evento1', start: "2020-04-11 20:00", end: "2020-04-12 02:00"},
+              {title: 'Evento1', start: "2020-04-13 00:00", end: "2020-04-14 02:00"},
+           ]
 
   constructor() { }
 
