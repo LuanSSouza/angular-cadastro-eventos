e683469ccdf94566e6532e2b6da61aaadb11aa6c
luanssouza <luan.souza27@hotmail.com>
2020-04-12 22:40:49 -0300

    logout

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  public isLogged: boolean;
 
-  constructor() { }
+  constructor(private auhtService: AuthService) { }
 
   ngOnInit(): void {
+    this.isLogged = this.auhtService.isLogged();
   }
+
+  loggout = () => this.auhtService.logout()
 
 }
