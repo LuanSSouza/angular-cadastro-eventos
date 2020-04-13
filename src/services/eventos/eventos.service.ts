9151eb58780763f78f0c8c07ebd9e15da2341d93
luanssouza <luan.souza27@hotmail.com>
2020-04-13 13:57:39 -0300

    refactoring event service

--- src/services/eventos/eventos.service.ts
+++ src/services/eventos/eventos.service.ts
@@ -10,9 +10,13 @@ export class EventosService {
 
   constructor(private authService: AuthService, private http: HttpClient) { }
 
-  public getEventos = () => this.http.get<Array<any>>(`${environment.apicalendario}/evento`, this.generateHeaders()).toPromise();
+  public getEventos = () => {
+    return this.http.get<Array<any>>(`${environment.apicalendario}/evento`, this.generateHeaders()).toPromise();
+  }
 
-  public deleteEvento = (id: number) => this.http.delete(`${environment.apicalendario}/evento/${id}`, this.generateHeaders()).toPromise();
+  public deleteEvento = (id: number) => {
+    return this.http.delete(`${environment.apicalendario}/evento/${id}`, this.generateHeaders()).toPromise();
+  }
 
   public postEvento = (evento: any) =>  {
     return this.http.post(`${environment.apicalendario}/evento`, evento, this.generateHeaders()).toPromise();
