cd12ceec541738529ce6038562cd40f676e7a472
luanssouza <luan.souza27@hotmail.com>
2020-04-13 12:23:44 -0300

    add message dialog

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmDialogComponent } from './dialog/confirm-dialog/confirm-dialog.component';
 import { MaterialModule } from '../material/material.module';
+import { MessageDialogComponent } from './dialog/message-dialog/message-dialog.component';
 
 
 
 @NgModule({
-  declarations: [ConfirmDialogComponent],
+  declarations: [ConfirmDialogComponent, MessageDialogComponent],
   imports: [ 
     CommonModule,
     MaterialModule,
