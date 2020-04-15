0c2923d8385a0e51a71e43e077b61614f292a943
luanssouza <luan.souza27@hotmail.com>
2020-04-15 15:27:01 -0300

    conviteservice

---
+++ src/services/convite/convite.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConviteService {
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
+
+  public getConvites = () => {
+    return this.http.get<any[]>(`${environment.apicalendario}/convite`, this.generateHeaders()).toPromise();
+  }
+
+  public postConvite = (convite) =>  {
+    return this.http.post(`${environment.apicalendario}/convite`, convite, this.generateHeaders()).toPromise();
+  }
+
+  public putConvite = (convite) =>  {
+    return this.http.put(`${environment.apicalendario}/convite`, convite, this.generateHeaders()).toPromise();
+  }
+
+  public deleteConvite = (convite) => {
+    return this.http.post(`${environment.apicalendario}/convite/deletar`, convite, this.generateHeaders()).toPromise();
+  }
+
+  private generateHeaders = () => {
+    return {
+      headers: new HttpHeaders(
+      { 'Content-Type': 'application/json', 'Authorization': `Bearer ${this.authService.getToken()}`} )
+    }
+  }
+}
