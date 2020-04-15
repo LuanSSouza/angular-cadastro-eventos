b904869e2de093d99d417cc1742b5966d273cadf
luanssouza <luan.souza27@hotmail.com>
2020-04-15 15:42:11 -0300

    add conviteForm

--- src/app/eventos/formulario-convites/formulario-convites.component.ts
+++ src/app/eventos/formulario-convites/formulario-convites.component.ts
@@ -1,4 +1,11 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsuarioService } from 'src/services/usuario/usuario.service';
+import { EventosService } from 'src/services/eventos/eventos.service';
+import { ConviteService } from 'src/services/convite/convite.service';
+import { Evento } from 'src/models/evento/evento.model';
+import { Usuario } from 'src/models/usuario/usuario.model';
 
 @Component({
   selector: 'app-formulario-convites',
@@ -6,10 +13,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario-convites.component.css']
 })
 export class FormularioConvitesComponent implements OnInit {
+  public conviteForm: FormGroup;
+  public eventos: Evento[] = [];
+  public usuarios: Usuario[] = [];
 
-  constructor() { }
+  constructor(private conviteService: ConviteService,
+    private eventosService: EventosService, 
+    private usuarioService: UsuarioService,
+    private location: Location) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    this.eventos = await this.eventosService.getEventos();
+    this.usuarios = await this.usuarioService.getAll();
+
+    this.conviteForm = new FormGroup({
+      evento: new FormControl('', [Validators.required]),
+      usuario: new FormControl('', [Validators.required]),
+    });
   }
 
+  public hasError = (controlName: string, errorName: string) =>{
+    return this.conviteForm.controls[controlName].hasError(errorName);
+  }
+
+  public onCancel = () => {
+    this.location.back();
+  }
+  
+  public submitLogin = (conviteFormValue) => {
+    if (this.conviteForm.valid) {
+      this.executeAdicionarCreation(conviteFormValue);
+    }
+  }
+ 
+  private executeAdicionarCreation = (form) => {
+    let convite: any = {
+      evento: form.evento,
+      usuario: form.usuario,
+      status: { id: 1 }
+    }
+    this.conviteService.postConvite(convite);
+  }
 }
